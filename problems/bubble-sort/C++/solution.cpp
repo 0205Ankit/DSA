59def272a9549544a11776fd01202ee94751ed3e
ankit <ankitbisht0205@gmail.com>
2024-03-31 00:14:31 +0530

    bubble sort c++ solution

--- problems/bubble-sort/C++/solution.cpp
+++ problems/bubble-sort/C++/solution.cpp
@@ -1,1 +1,33 @@
 // Push the maximum to last by adjacent swaps
+// NOTE: can also be done using 2 loops like the solution of selection sort instead of using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bubbleSort(vector<int> arr, int iteration)
+{
+
+    if (iteration > arr.size() - 2)
+        return arr;
+
+    for (int i = 0; i <= arr.size() - 2; i++)
+    {
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+    };
+
+    return bubbleSort(arr, iteration + 1);
+}
+
+int main()
+{
+    // Write C++ code here
+
+    vector<int> arr = {3, 12, 8, 4, 11, 1, 25};
+
+    vector<int> sortedArr = bubbleSort(arr, 0);
+
+    for (auto &element : sortedArr)
+        cout << element << " ";
+
+    return 0;
+}
