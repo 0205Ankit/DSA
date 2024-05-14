67397d81335b1a7b72a212c91815cc45dabe04b6
ankit <ankitbisht0205@gmail.com>
2024-05-14 18:19:19 +0530

    0268-Missing-number

---
+++ problems/0268-Missing-number/C++/solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = (n * (n + 1)) / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum - nums[i];
+        };
+
+        return sum;
+    }
+};
